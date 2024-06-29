9b63fa8498afdc696a229c1e9fdc0777254e08ae
Dr. Zero <162797786+encryptedskull@users.noreply.github.com>
2024-06-29 18:15:08 +0500

    fixed some issues

--- new_snippet.js
+++ new_snippet.js
@@ -2,20 +2,24 @@ const scBtn = document.getElementById("save-create");
 const homeBtn = document.getElementById("btn-home");
 const titleField = document.querySelector(".title-field");
 const codeField = document.querySelector(".code-field");
-
+const sMsg = document.querySelector(".msg-div");
 const snippets = JSON.parse(localStorage.getItem("snippets") ?? "[]");
 
 scBtn.addEventListener("click", function () {
     const titleValue = titleField.value;
     const codeValue = codeField.value;
+    if (!titleValue || !codeValue) return;
+
     const data = {
         title: titleValue,
         code: codeValue,
     };
+
     snippets.push(data);
+    localStorage.setItem("snippets", JSON.stringify(snippets));
     titleField.value = "";
     codeField.value = "";
-    localStorage.setItem("snippets", JSON.stringify(snippets));
+    // sMsg.style.display = "block";
 });
 
 homeBtn.addEventListener("click", function () {
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const snippetsContainer = document.querySelector(".snippets");
 newBtn.addEventListener("click", function () {
     window.location.href = "n_snippet.html";
 });
-const snippets = JSON.parse(localStorage.getItem("snippets") ?? "[]");
+let snippets = JSON.parse(localStorage.getItem("snippets") ?? "[]");
 
 snippets.forEach((item, i) => {
     // console.log("outer", i);
@@ -36,11 +36,53 @@ snippets.forEach((item, i) => {
         window.location.href = "p_snippet.html";
     });
 
-    deleteBtn.addEventListener("click", function () {
-        // console.log("delete");
-        snippets.splice(dId, 1);
-        localStorage.setItem("snippets", JSON.stringify(snippets));
-        // window.location.reload();
-        snippetItem.remove();
-    });
+    // deleteBtn.addEventListener("click", function () {
+    //     snippets = snippets.filter((_, index) => index !== Number(dId));
+    //     rerenderElement(snippets);
+    //     localStorage.setItem("snippets", JSON.stringify(snippets));
+    //     console.log("clicked");
+    // });
 });
+rerenderElement(snippets);
+function rerenderElement(snippets) {
+    snippetsContainer.innerHTML = "";
+    snippets.forEach((item, i) => {
+        // console.log("outer", i);
+
+        const snippetItem = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        const titleDiv = document.createElement("div");
+
+        titleDiv.setAttribute("class", "snippet-title");
+        deleteBtn.setAttribute("class", "btn-del");
+        snippetItem.setAttribute("class", "snippet-item");
+        snippetItem.setAttribute("data-id", i);
+
+        const dId = snippetItem.getAttribute("data-id");
+
+        // console.log(item.id);
+        const updated = JSON.stringify(snippets);
+
+        localStorage.setItem("snippets", updated);
+
+        titleDiv.textContent = `${i + 1}. ${item.title}`;
+        deleteBtn.textContent = "Delete";
+        snippetItem.appendChild(titleDiv);
+        snippetItem.appendChild(deleteBtn);
+        snippetsContainer.appendChild(snippetItem);
+        titleDiv.addEventListener("click", function () {
+            localStorage.setItem("previewID", dId);
+            window.location.href = "p_snippet.html";
+        });
+        deleteBtn.addEventListener("click", function () {
+            snippets = snippets.filter((_, index) => index !== Number(dId));
+            rerenderElement(snippets);
+            localStorage.setItem("snippets", JSON.stringify(snippets));
+            // console.log("clicked");
+        });
+    });
+}
+
+const getData = () => {};
+
+getData();
